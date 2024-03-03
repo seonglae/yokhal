a79fe337039758f42b5780c46a9fc7595aa6e7c8
seonglae <sungle3737@gmail.com>
2024-03-03 16:33:13 +0000

    feat: train with qlora with tokenizer template test

--- train_sft_qlora.py
+++ train_sft_qlora.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass, field
 from typing import Optional
+import re
+import os
 
 import torch
-
 from transformers import AutoTokenizer, HfArgumentParser, AutoModelForCausalLM, BitsAndBytesConfig, TrainingArguments
 from datasets import load_dataset
 from peft import LoraConfig
@@ -13,8 +14,8 @@ class ScriptArguments:
     """
     These arguments vary depending on how many GPUs you have, what their capacity and features are, and what size model you want to train.
     """
-    per_device_train_batch_size: Optional[int] = field(default=4)
-    per_device_eval_batch_size: Optional[int] = field(default=1)
+    per_device_train_batch_size: Optional[int] = field(default=2)
+    per_device_eval_batch_size: Optional[int] = field(default=2)
     gradient_accumulation_steps: Optional[int] = field(default=4)
     learning_rate: Optional[float] = field(default=2e-4)
     max_grad_norm: Optional[float] = field(default=0.3)
@@ -22,7 +23,7 @@ class ScriptArguments:
     lora_alpha: Optional[int] = field(default=16)
     lora_dropout: Optional[float] = field(default=0.1)
     lora_r: Optional[int] = field(default=8)
-    max_seq_length: Optional[int] = field(default=2048)
+    max_seq_length: Optional[int] = field(default=1024)
     model_name: Optional[str] = field(
         default=None,
         metadata={
@@ -30,11 +31,11 @@ class ScriptArguments:
         }
     )
     dataset_name: Optional[str] = field(
-        default="stingning/ultrachat",
+        default="heegyu/open-korean-instructions",
         metadata={"help": "The preference dataset to use."},
     )
     fp16: Optional[bool] = field(
-        default=False,
+        default=True,
         metadata={"help": "Enables fp16 training."},
     )
     bf16: Optional[bool] = field(
@@ -61,9 +62,9 @@ class ScriptArguments:
         default="constant",
         metadata={"help": "Learning rate schedule. Constant a bit better than cosine, and has advantage for analysis"},
     )
-    max_steps: int = field(default=1000, metadata={"help": "How many optimizer update steps to take"})
+    max_steps: int = field(default=100000, metadata={"help": "How many optimizer update steps to take"})
     warmup_ratio: float = field(default=0.03, metadata={"help": "Fraction of steps to do a warmup for"})
-    save_steps: int = field(default=10, metadata={"help": "Save checkpoint every X updates steps."})
+    save_steps: int = field(default=100, metadata={"help": "Save checkpoint every X updates steps."})
     logging_steps: int = field(default=10, metadata={"help": "Log every X updates steps."})
     output_dir: str = field(
         default="./results",
@@ -74,12 +75,29 @@ parser = HfArgumentParser(ScriptArguments)
 script_args = parser.parse_args_into_dataclasses()[0]
 
 
-def formatting_func(example):
-    text = f"### USER: {example['data'][0]}\n### ASSISTANT: {example['data'][1]}"
-    return text
+def formatting_func(row):
+    text = row['text']
+    text = text.replace('<|user|>', '')
+    text = text.replace('<|bot|>', '')
+    pattern = re.compile(r'(<sys>|<usr>|<bot>)(.*?)(?=<sys>|<usr>|<bot>|$)', re.DOTALL)
+    matches = pattern.findall(text)
+    chat = []
+    roles = {"<sys>": "assistant", "<usr>": "user", "<bot>": "assistant"}
+    for token, content in matches:
+      chat.append({"role": roles[token], "content": content.strip()})
+    if (len(chat) == 0): return {"text": ""}
+    if (len(chat) == 1): return {"text": ''}
+    if (chat[0]['role'] == 'assistant'):
+      chat[1]['content'] = f"{chat[0]['content']}\n{chat[1]['content']}"
+      chat = chat[1:]
+    try:
+      prompt = tokenizer.apply_chat_template(chat, tokenize=False)
+    except Exception:
+      prompt = ""
+    return {"text": prompt}
 
 # Load the GG model - this is the local one, update it to the one on the Hub
-model_id = "google/gemma-7b"
+model_id = "./hannam-2b/checkpoint-27000"
 
 quantization_config = BitsAndBytesConfig(
     load_in_4bit=True,
@@ -91,7 +109,7 @@ quantization_config = BitsAndBytesConfig(
 model = AutoModelForCausalLM.from_pretrained(
     model_id, 
     quantization_config=quantization_config, 
-    torch_dtype=torch.float32,
+    torch_dtype=torch.float16,
     attn_implementation="sdpa" if not script_args.use_flash_attention_2 else "flash_attention_2"
 )
 
@@ -108,11 +126,17 @@ lora_config = LoraConfig(
     lora_dropout=script_args.lora_dropout
 )
 
-train_dataset = load_dataset(script_args.dataset_name, split="train[:5%]")
+train_dataset = load_dataset(script_args.dataset_name, split="train")
+train_dataset = train_dataset.map(
+    formatting_func,
+    num_proc=os.cpu_count() // 2,
+)
+train_dataset = train_dataset.filter(lambda x: len(x["text"]) > 0)
+print(train_dataset[0])
 
-# TODO: make that configurable
-YOUR_HF_USERNAME = ''
-output_dir = f"{YOUR_HF_USERNAME}/gemma-qlora-ultrachat"
+
+YOUR_HF_USERNAME = 'seonglae'
+output_dir = f"{YOUR_HF_USERNAME}/hannam-chat"
 
 training_arguments = TrainingArguments(
     output_dir=output_dir,
@@ -136,14 +160,14 @@ trainer = SFTTrainer(
     args=training_arguments,
     train_dataset=train_dataset,
     peft_config=lora_config,
+    dataset_text_field="text",
     packing=script_args.packing,
-    dataset_text_field="id",
     tokenizer=tokenizer,
     max_seq_length=script_args.max_seq_length,
-    formatting_func=formatting_func,
 )
 
 trainer.train()
+trainer.save_model(output_dir)
 model.save_pretrained('hannam-2b')
 tokenizer.save_pretrained('hannam-2b')
 tokenizer.save_vocabulary('hannam-2b')
