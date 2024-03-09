5c4cf234efb8e53d36790f4442355019f4a23fc1
seonglae <sungle3737@gmail.com>
2024-03-10 02:11:37 +0900

    refactor: ruff formatting with train.py class member

--- train.py
+++ train.py
@@ -1,10 +1,9 @@
-import torch
-import fire
 import os
 import logging
 
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, PreTrainedModel, HfArgumentParser, TrainingArguments
+import fire
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, PreTrainedModel, HfArgumentParser
 from datasets import load_dataset
 from peft import LoraConfig
 from trl import SFTTrainer
@@ -17,24 +16,27 @@ torch.manual_seed(0)
 
 
 class yokhalTrainer():
+  model: PreTrainedModel
+  tokenizer: AutoTokenizer
+
   def finetune(self, base='seonglae/yokhal-md', save_local=True, push=False, resume=False,
                epoch=1, batch=3, output="./yokhal-md", target="wiki", max_length=1024,
                log_steps=10, save_steps=100, eval_steps=100, lr=1e-5, optim='adafactor'):
 
     # Load the dataset and format it for training.
     train_ds, eval_ds = get_dataset(target)
-    print(f"Train data: {len(train_ds)} Eval data: {len(eval_ds)}")
+    logging.info(f"Train data: {len(train_ds)} Eval data: {len(eval_ds)}")
 
     # Load the pretrained model and tokenizer.
-    tokenizer = AutoTokenizer.from_pretrained(base)
-    model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(base,
-                                                                  torch_dtype=torch.bfloat16,
-                                                                  device_map="auto")
-    print(f'Special tokens: {tokenizer.all_special_tokens}')
+    self.tokenizer = AutoTokenizer.from_pretrained(base)
+    self.model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(base,
+                                                                       torch_dtype=torch.bfloat16,
+                                                                       device_map="auto")
+    logging.info(f'Special tokens: {self.tokenizer.all_special_tokens}')
 
     # Finally, set up the trainer and train the model.
     trainer = SFTTrainer(
-        model=model,
+        model=self.model,
         train_dataset=train_ds,
         eval_dataset=eval_ds,
         args=TrainingArguments(
@@ -54,75 +56,76 @@ class yokhalTrainer():
         packing=True,
     )
     trainer.train(resume_from_checkpoint=resume)
-    self.push(model, tokenizer, output, save_local=save_local, push=push)
+    self.push(output, save_local=save_local, push=push)
 
   def adapt(self, model_id='seonglae/yokhal-md', save_local=True, push=False,
-               epoch=1, batch=3, output="./yokhal-md"):
+            epoch=1, batch=3, output="./yokhal-md"):
     parser = HfArgumentParser(ScriptArguments)
     script_args = parser.parse_args_into_dataclasses()[0]
-    model = AutoModelForCausalLM.from_pretrained(model_id, 
-      quantization_config=quantization_config, 
-      torch_dtype=torch.float16,
-      attn_implementation="sdpa" if not script_args.use_flash_attention_2 else "flash_attention_2"
-    )
+    model = AutoModelForCausalLM.from_pretrained(model_id,
+                                                 quantization_config=quantization_config,
+                                                 torch_dtype=torch.float16,
+                                                 attn_implementation="sdpa" if not script_args.use_flash_attention_2 else "flash_attention_2"
+                                                 )
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     # tokenizer.pad_token_id = tokenizer.eos_token_id
     lora_config = LoraConfig(
-      r=script_args.lora_r,
-      target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"],
-      bias="none",
-      task_type="CAUSAL_LM",
-      lora_alpha=script_args.lora_alpha,
-      lora_dropout=script_args.lora_dropout
+        r=script_args.lora_r,
+        target_modules=["q_proj", "o_proj", "k_proj",
+                        "v_proj", "gate_proj", "up_proj", "down_proj"],
+        bias="none",
+        task_type="CAUSAL_LM",
+        lora_alpha=script_args.lora_alpha,
+        lora_dropout=script_args.lora_dropout
     )
     formatting_func = make_format_func(model_id)
     train_dataset = load_dataset(script_args.dataset_name, split="train")
     train_dataset = train_dataset.map(
-      formatting_func,
-      num_proc=os.cpu_count() // 2,
+        formatting_func,
+        num_proc=os.cpu_count() // 2,
     )
     train_dataset = train_dataset.filter(lambda x: len(x["text"]) > 0)
     logging.info(train_dataset[0])
 
     training_arguments = TrainingArguments(
-      output_dir=output,
-      per_device_train_batch_size=batch,
-      per_device_eval_batch_size=batch,
-      gradient_accumulation_steps=script_args.gradient_accumulation_steps,
-      optim=script_args.optim,
-      save_steps=script_args.save_steps,
-      logging_steps=script_args.logging_steps,
-      learning_rate=script_args.learning_rate,
-      num_train_epochs=epoch,
-      max_grad_norm=script_args.max_grad_norm,
-      max_steps=script_args.max_steps,
-      warmup_ratio=script_args.warmup_ratio,
-      lr_scheduler_type=script_args.lr_scheduler_type,
-      gradient_checkpointing=script_args.gradient_checkpointing,
-      fp16=script_args.fp16,
-      bf16=script_args.bf16,
+        output_dir=output,
+        per_device_train_batch_size=batch,
+        per_device_eval_batch_size=batch,
+        gradient_accumulation_steps=script_args.gradient_accumulation_steps,
+        optim=script_args.optim,
+        save_steps=script_args.save_steps,
+        logging_steps=script_args.logging_steps,
+        learning_rate=script_args.learning_rate,
+        num_train_epochs=epoch,
+        max_grad_norm=script_args.max_grad_norm,
+        max_steps=script_args.max_steps,
+        warmup_ratio=script_args.warmup_ratio,
+        lr_scheduler_type=script_args.lr_scheduler_type,
+        gradient_checkpointing=script_args.gradient_checkpointing,
+        fp16=script_args.fp16,
+        bf16=script_args.bf16,
     )
     trainer = SFTTrainer(
-      model=model,
-      args=training_arguments,
-      train_dataset=train_dataset,
-      peft_config=lora_config,
-      dataset_text_field="text",
-      packing=script_args.packing,
-      tokenizer=tokenizer,
-      max_seq_length=script_args.max_seq_length,
+        model=model,
+        args=training_arguments,
+        train_dataset=train_dataset,
+        peft_config=lora_config,
+        dataset_text_field="text",
+        packing=script_args.packing,
+        tokenizer=tokenizer,
+        max_seq_length=script_args.max_seq_length,
     )
     trainer.train(resume_from_checkpoint=True)
     self.push(model, tokenizer, output, save_local=save_local, push=push)
 
-  def push(self, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, output, save_local, push):
+  def push(self, output, save_local, push):
     if save_local:
-      model.save_pretrained(output)
-      tokenizer.save_pretrained(output)
+      self.model.save_pretrained(output)
+      self.tokenizer.save_pretrained(output)
     if push:
-      model.name_or_path = push
-      model.push_to_hub(push)
-      tokenizer.push_to_hub(push)
+      self.model.name_or_path = push
+      self.model.push_to_hub(push)
+      self.tokenizer.push_to_hub(push)
 
 
 if __name__ == '__main__':
